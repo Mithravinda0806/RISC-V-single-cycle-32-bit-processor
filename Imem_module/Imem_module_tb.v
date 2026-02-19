6b8eb2c29b18d06fa1d33ecd51c5ebdc7247f361
Mithravinda0806 <168867151+Mithravinda0806@users.noreply.github.com>
2026-02-19 22:35:57 +0530

    Imem_module_tb.v

--- Imem_module/Imem_module_tb.v
+++ Imem_module/Imem_module_tb.v
@@ -1,3 +1,5 @@
+`timescale 1ns / 1ps
+
 module instr_mem_tb;
 
     reg  [31:0] addr;
@@ -18,44 +20,3 @@ module instr_mem_tb;
     end
 
 endmodule
-//module instr_mem_tb;
-
-    reg  [31:0] addr;
-    wire [31:0] instr;
-
-    instr_mem dut (
-        .addr(addr),
-        .instr(instr)
-    );
-
-    initial begin
-        addr = 32'd0;    #10;   // instruction 0
-        addr = 32'd4;    #10;   // instruction 1
-        addr = 32'd8;    #10;   // instruction 2
-        addr = 32'd12;   #10;   // instruction 3
-        addr = 32'd16;   #10;   // instruction 4
-        $finish;
-    end
-
-endmodule
-//module instr_mem_tb;
-
-    reg  [31:0] addr;
-    wire [31:0] instr;
-
-    instr_mem dut (
-        .addr(addr),
-        .instr(instr)
-    );
-
-    initial begin
-        addr = 32'd0;    #10;   // instruction 0
-        addr = 32'd4;    #10;   // instruction 1
-        addr = 32'd8;    #10;   // instruction 2
-        addr = 32'd12;   #10;   // instruction 3
-        addr = 32'd16;   #10;   // instruction 4
-        $finish;
-    end
-
-endmodule
-//PC is byte-addressed, instruction memory is word-addressed.Dividing PC by 4 converts byte address to instruction index.//
